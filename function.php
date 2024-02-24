1a3b0b27df6672c37987f932d120ec1e9ab1deaf
Farbod akvan <farbodezrael@gmail.com>
2024-02-24 15:04:01 +0330

    Update function.php

--- function.php
+++ function.php
@@ -1,6 +1,7 @@
 /*** ADD ESTIMATED READING TIME ***/
 function reading_time() {
-    $content = get_post_field( 'post_content', $post->ID );
+    $post_id = get_the_ID();
+    $content = get_post_field( 'post_content', $post_id );
     $word_count = explode(" ",$content);
 	$all_word = count($word_count);
     $readingtime = ceil($all_word / 300);
